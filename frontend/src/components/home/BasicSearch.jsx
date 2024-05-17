a88784d5bec25de4bc3fbedd57e00a6d84f80ded
Adrian <adrian@MacBook-Pro-de-Adrian.local>
2024-05-16 21:07:51 -0700

    chagned and updated (ig)

--- frontend/src/components/home/BasicSearch.jsx
+++ frontend/src/components/home/BasicSearch.jsx
@@ -1,46 +1,74 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useCallback } from "react";
 import BlogCard from "../search/BlogCard";
 import { getFeaturedPosts, getTags } from "../../services/functions";
 import TagLabel from "../search/TagLabel";
 import { RiAccountCircleFill } from "react-icons/ri";
+import LoadingSpinner from "../LoadingSpinner";
 
 const SearchList = () => {
   const [blogs, setBlogs] = useState([]);
   const [filteredBlogs, setFilteredBlogs] = useState(undefined);
 
+  //ref
+  const inputRef = useRef(null);
+
   // filters
   const [tags, setTags] = useState([]);
-  const inputRef = useRef(null);
   const [selectedTags, setSelectedTags] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
 
   // View More
   const [viewMoreTags, setViewMoreTags] = useState(false);
 
-  const getBlogs = async () => {
-    const posts = await getFeaturedPosts(selectedTags.map((tag) => tag.id));
-    setBlogs(posts.data);
-    setFilteredBlogs(posts.data);
-  };
-  const getServerTags = async () => {
+  const getBlogs = useCallback(
+    async (selectedTagIds) => {
+      const posts = await getFeaturedPosts(selectedTagIds);
+      setBlogs(posts.data);
+      setFilteredBlogs(posts.data);
+      return posts.data;
+    },
+    [selectedTags]
+  );
+
+  const getServerTags = useCallback(async () => {
     const fetchedTags = await getTags();
     setTags(fetchedTags);
-  };
+  }, []);
 
-  const searchBlogs = () => {
-    const searchValue = inputRef.current.value;
-    const filteredblogs = blogs.filter((blog) => {
-      return blog.name.toLowerCase().includes(searchValue.toLowerCase());
-    });
-    setFilteredBlogs(filteredblogs);
-  };
+  const filterBlogsBySearchQuery = useCallback(
+    (currBlogs) => {
+      if (searchQuery === "") return currBlogs;
+      const filteredblogs = currBlogs.filter((blog) => {
+        return blog.titulo.toLowerCase().includes(searchQuery.toLowerCase());
+      });
+      return filteredblogs;
+    },
+    [searchQuery]
+  );
+
+  const filterBlogs = useCallback(async () => {
+    const filters = {
+      tags: selectedTags.map((tag) => tag.id),
+    };
+    const currBlogs = await getBlogs(filters);
+    const filteredBySearchQueryBlogs = filterBlogsBySearchQuery(currBlogs);
+    setFilteredBlogs(filteredBySearchQueryBlogs);
+  }, [selectedTags, searchQuery]);
 
   useEffect(() => {
     getServerTags();
   }, []);
 
   useEffect(() => {
-    getBlogs();
+    const filteredBySearchQueryBlogs = filterBlogsBySearchQuery(blogs);
+    setFilteredBlogs(filteredBySearchQueryBlogs);
+  }, [searchQuery]);
+
+  useEffect(() => {
+    filterBlogs();
   }, [selectedTags]);
+
+  console.log(blogs);
 
   return (
     <div className=" max-w-[1400px] w-full mx-auto justify-center flex ">
@@ -66,11 +94,8 @@ const SearchList = () => {
               </svg>
             </div>
             <input
-              ref={inputRef}
-              onKeyDown={(e) => {
-                if (e.key === "Enter") {
-                  searchBlogs();
-                }
+              onChange={(e) => {
+                setSearchQuery(e.target.value);
               }}
               type="search"
               className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-transparent focus:ring-none focus:border-gray-50 focus:outline-slate-300 focus:outline-[1px]"
@@ -81,27 +106,7 @@ const SearchList = () => {
         {/* blogs */}
         <div className="flex w-full mt-4 justify-center">
           <div className="w-full  md:px-10  justify-center items-center flex flex-col ">
-            {filteredBlogs == undefined && (
-              <div role="status" className="mt-[50px]">
-                <svg
-                  aria-hidden="true"
-                  className="w-12 h-12 text-gray-200 animate-spin dark:text-black fill-[#7678FF]"
-                  viewBox="0 0 100 101"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                    fill="currentColor"
-                  />
-                  <path
-                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                    fill="currentFill"
-                  />
-                </svg>
-                <span className="sr-only">Loading...</span>
-              </div>
-            )}
+            {filteredBlogs == undefined && <LoadingSpinner />}
             {filteredBlogs &&
               filteredBlogs.map((blog, index) => {
                 return <BlogCard key={index} blog={blog} />;
@@ -114,7 +119,12 @@ const SearchList = () => {
       <div className="w-3/5 max-h-[calc(100vh-60px)] overflow-scroll hidden md:flex flex-col px-4 py-6  border-l-[1px] border-gray-200  gap-6 scrollbar-hide">
         {/* Tags */}
         <div>
-          <button className="underline text-sm hover:text-[#7678FFFF]">
+          <button
+            className="underline text-sm hover:text-[#7678FFFF]"
+            onClick={() => {
+              setSelectedTags([]);
+            }}
+          >
             Limpiar Filtros
           </button>
           <h1 className="font-medium">Tags Recomendados</h1>
@@ -125,10 +135,14 @@ const SearchList = () => {
                 return (
                   <div
                     onClick={() => {
-                      if (selectedTags.includes(tag)) {
+                      if (
+                        selectedTags.some(
+                          (selectedTag) => selectedTag.tag === tag.tag
+                        )
+                      ) {
                         const updatedSelectedTags = selectedTags.filter(
                           (filterTag) => {
-                            return filterTag != tag;
+                            return filterTag.tag != tag.tag;
                           }
                         );
                         setSelectedTags(updatedSelectedTags);
@@ -138,11 +152,15 @@ const SearchList = () => {
                       }
                     }}
                     className="cursor-pointer"
+                    key={index}
                   >
                     <TagLabel
-                      key={index}
                       tag={tag.tag}
-                      disabled={!selectedTags.includes(tag)}
+                      disabled={
+                        !selectedTags.some(
+                          (selectedTag) => selectedTag.tag === tag.tag
+                        )
+                      }
                     />
                   </div>
                 );
@@ -153,10 +171,14 @@ const SearchList = () => {
                 return (
                   <div
                     onClick={() => {
-                      if (selectedTags.includes(tag)) {
+                      if (
+                        selectedTags.some(
+                          (selectedTag) => selectedTag.tag === tag.tag
+                        )
+                      ) {
                         const updatedSelectedTags = selectedTags.filter(
                           (filterTag) => {
-                            return filterTag != tag;
+                            return filterTag.tag != tag.tag;
                           }
                         );
                         setSelectedTags(updatedSelectedTags);
@@ -170,7 +192,11 @@ const SearchList = () => {
                     <TagLabel
                       key={index}
                       tag={tag.tag}
-                      disabled={!selectedTags.includes(tag)}
+                      disabled={
+                        !selectedTags.some(
+                          (selectedTag) => selectedTag.tag === tag.tag
+                        )
+                      }
                     />
                   </div>
                 );
@@ -186,6 +212,8 @@ const SearchList = () => {
             {viewMoreTags ? "Ver menos tags" : "Ver mas tags"}
           </button>
         </div>
+
+        {/* Autores */}
       </div>
     </div>
   );
