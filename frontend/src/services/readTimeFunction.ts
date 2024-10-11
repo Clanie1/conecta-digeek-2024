b4e3a23496f97cf3e69796cc39a8c298fdff7211
clanie1 <danybarociov@gmail.com>
2024-10-11 11:22:29 -0700

    ts ignore debounce

--- frontend/src/services/readTimeFunction.ts
+++ frontend/src/services/readTimeFunction.ts
@@ -1,11 +1,10 @@
-export default function readTimeFunction(content) : number{
-    // Eliminar espacios en blanco al principio y al final del texto
-    content = content.trim();
+export default function readTimeFunction(content: string): number {
+  // Eliminar espacios en blanco al principio y al final del texto
+  content = content.trim();
 
-    // Dividir el texto en palabras usando los espacios en blanco como separadores
-    const palabras: string[] = content.split(/\s+/);
+  // Dividir el texto en palabras usando los espacios en blanco como separadores
+  const palabras: string[] = content.split(/\s+/);
 
-    // Retornar la cantidad de palabras encontradas
-    return palabras.length;
-    
+  // Retornar la cantidad de palabras encontradas
+  return palabras.length;
 }
--- frontend/src/util/images/utils.ts
+++ frontend/src/util/images/utils.ts
@@ -3,8 +3,10 @@ export function trim(text: string, length: number): string {
 }
 
 export function debounce(func: Function, wait: number): Function {
+  // @ts-ignore
   let timeout: NodeJS.Timeout | null;
   return function (...args: any[]) {
+    // @ts-ignore
     const context = this as any;
     const later = () => {
       timeout = null;
